3c4bf2de8a8186ccbd9940a16780465c4fa7b0bb
Michael S <michael@schmut.de>
2019-04-01 19:37:50 +0000

    Update ampel.py

--- doc/Stunden/Ampel/ampel.py
+++ doc/Stunden/Ampel/ampel.py
@@ -1,12 +1,12 @@
 from time import sleep
 from RPi import GPIO
 
-PKW_GRUEN = 17
-PKW_GELB = 18
-PKW_ROT = 19
+PKW_GRUEN = 5
+PKW_GELB = 6
+PKW_ROT = 13
 
-FUSSGAENGER_GRUEN = 20
-FUSSGAENGER_ROT = 21
+FUSSGAENGER_GRUEN = 17
+FUSSGAENGER_ROT = 27
 
 class Ampel:
     def __init__(self):
@@ -15,8 +15,16 @@ class Ampel:
         GPIO.setup(PKW_GRUEN, GPIO.OUT)
         GPIO.setup(PKW_GELB, GPIO.OUT)
         GPIO.setup(PKW_ROT, GPIO.OUT)
+        
         GPIO.setup(FUSSGAENGER_GRUEN, GPIO.OUT)
         GPIO.setup(FUSSGAENGER_ROT, GPIO.OUT)
+
+        GPIO.output(PKW_GRUEN, GPIO.HIGH)
+        GPIO.output(PKW_GELB, GPIO.LOW)
+        GPIO.output(PKW_ROT, GPIO.LOW)
+        
+        GPIO.output(FUSSGAENGER_GRUEN, GPIO.LOW)
+        GPIO.output(FUSSGAENGER_ROT, GPIO.HIGH)
 
     def fussgaengerGruen(self):
         GPIO.output(FUSSGAENGER_ROT, GPIO.LOW)
