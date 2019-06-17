0f8c9870f8d0c1f5af4a2886ff3e216b0d3ef564
Michael S <michael@schmut.de>
2019-06-17 23:22:46 +0200

    Update temperatur.py

--- doc/Stunden/temperatur.py
+++ doc/Stunden/temperatur.py
@@ -3,37 +3,34 @@ import time, sys
 from status2 import AnzeigeKurz 
 
 sensor = '/sys/bus/w1/devices/28-041670d740ff/w1_slave'
- 
-def readTempSensor(sensorName) :
-    f = open(sensorName, 'r')
+
+anzeige = AnzeigeKurz()
+anzeige.define_range(23, 30)
+
+def readSensor() :
+    f = open(sensor, 'r')
     lines = f.readlines()
     f.close()
     return lines
  
-def readTempLines(sensorName) :
-    lines = readTempSensor(sensorName)
+def readTemperature() :
+    lines = readSensor()
     while lines[0].strip()[-3:] != 'YES':
         time.sleep(0.2)
-        lines = readTempSensor(sensorName)
+        lines = readSensor()
     temperaturStr = lines[1].find('t=')
     if temperaturStr != -1 :
         tempData = lines[1][temperaturStr+2:]
-        tempCelsius = float(tempData) / 1000.0
-        return tempCelsius
- 
-anzeige = AnzeigeKurz()
-anzeige.define_range(23, 30)
+        tempValue = float(tempData) / 1000.0
+        return tempValue
+
 try:
     while True :
-        temp = readTempLines(sensor)
+        temp = readTemperature()
         anzeige.range_status(temp)
         print("Temperatur: " + str(temp) + " °C")
         time.sleep(10)
 except KeyboardInterrupt:
-    print('Temperaturmessung wird beendet')
-except Exception as e:
-    print(str(e))
-    sys.exit(1)
+    print('Programm wird beendet')
 finally:
-    print('Programm wird beendet.')
     sys.exit(0)
